78f82013a1efd237399f78ab7992bfdab2383737
AxDevv <twitch.rwayl@gmail.com>
2026-01-12 05:55:22 +0100

    Fix Folia detection and add PacketEvents dependency
    - Simplify Folia detection using RegionizedServer class
    - Remove AsyncScheduler dependency (not available in Canvas)
    - Use ScheduledExecutorService for async tasks on Folia
    - Add PacketEvents to plugin dependencies
    - Improve error handling in FoliaCompat

--- triton-spigot/src/main/java/com/rexcantor64/triton/spigot/utils/FoliaCompat.java
+++ triton-spigot/src/main/java/com/rexcantor64/triton/spigot/utils/FoliaCompat.java
@@ -7,46 +7,32 @@ import org.bukkit.plugin.Plugin;
 
 import java.lang.reflect.Method;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ThreadFactory;
 
 public final class FoliaCompat {
 
     private static final boolean IS_FOLIA;
-    private static Method getAsyncScheduler;
-    private static Method getGlobalRegionScheduler;
-    private static Method getRegionScheduler;
-    private static Method asyncRunNow;
-    private static Method asyncRunAtFixedRate;
-    private static Method globalRun;
-    private static Method globalRunDelayed;
-    private static Method globalExecute;
-    private static Method regionRun;
-    private static Method regionRunDelayed;
+    private static final ScheduledExecutorService ASYNC_EXECUTOR = Executors.newScheduledThreadPool(4, new ThreadFactory() {
+        private int count = 0;
+
+        @Override
+        public Thread newThread(Runnable r) {
+            return new Thread(r, "Triton-Async-" + (count++));
+        }
+    });
 
     static {
         boolean folia = false;
         try {
-            Class.forName("io.papermc.paper.threadedregions.ThreadedRegionizer");
+            Class.forName("io.papermc.paper.threadedregions.RegionizedServer");
             folia = true;
-            getAsyncScheduler = Bukkit.class.getMethod("getAsyncScheduler");
-            getGlobalRegionScheduler = Bukkit.class.getMethod("getGlobalRegionScheduler");
-            getRegionScheduler = Bukkit.class.getMethod("getRegionScheduler");
-
-            Class<?> asyncScheduler = Class.forName("io.papermc.paper.scheduler.AsyncScheduler");
-            asyncRunNow = asyncScheduler.getMethod("runNow", Plugin.class, Runnable.class);
-            asyncRunAtFixedRate = asyncScheduler.getMethod("runAtFixedRate", Plugin.class, Runnable.class, long.class, long.class, TimeUnit.class);
-
-            Class<?> regionScheduler = Class.forName("io.papermc.paper.scheduler.RegionScheduler");
-            regionRun = regionScheduler.getMethod("run", Plugin.class, Location.class, Runnable.class);
-            regionRunDelayed = regionScheduler.getMethod("runDelayed", Plugin.class, Location.class, Runnable.class, long.class);
-
-            Class<?> globalRegionScheduler = Class.forName("io.papermc.paper.scheduler.GlobalRegionScheduler");
-            globalRun = globalRegionScheduler.getMethod("run", Plugin.class, Runnable.class);
-            globalRunDelayed = globalRegionScheduler.getMethod("runDelayed", Plugin.class, Runnable.class, long.class);
-            globalExecute = globalRegionScheduler.getMethod("execute", Plugin.class, Runnable.class);
-        } catch (ClassNotFoundException | NoSuchMethodException ignored) {
+        } catch (ClassNotFoundException ignored) {
             folia = false;
         }
         IS_FOLIA = folia;
+        System.out.println("[Triton] Folia detection: " + folia);
     }
 
     public static boolean isFolia() {
@@ -55,35 +41,23 @@ public final class FoliaCompat {
 
     public static void runAsync(Plugin plugin, Runnable runnable) {
         if (IS_FOLIA) {
-            try {
-                Object scheduler = getAsyncScheduler.invoke(Bukkit.getServer());
-                asyncRunNow.invoke(scheduler, plugin, runnable);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            ASYNC_EXECUTOR.execute(runnable);
         } else {
             Bukkit.getScheduler().runTaskAsynchronously(plugin, runnable);
         }
     }
 
     public static void runDelayed(Plugin plugin, Runnable runnable, long delay) {
-        if (IS_FOLIA) {
-            try {
-                Object scheduler = getGlobalRegionScheduler.invoke(Bukkit.getServer());
-                globalRunDelayed.invoke(scheduler, plugin, runnable, delay);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        } else {
-            Bukkit.getScheduler().runTaskLater(plugin, runnable, delay);
-        }
+        run(plugin, runnable);
     }
 
     public static void run(Plugin plugin, Runnable runnable) {
         if (IS_FOLIA) {
             try {
-                Object scheduler = getGlobalRegionScheduler.invoke(Bukkit.getServer());
-                globalRun.invoke(scheduler, plugin, runnable);
+                Method method = Bukkit.class.getMethod("getGlobalRegionScheduler");
+                Object scheduler = method.invoke(Bukkit.getServer());
+                method = scheduler.getClass().getMethod("execute", Plugin.class, Runnable.class);
+                method.invoke(scheduler, plugin, runnable);
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -93,42 +67,15 @@ public final class FoliaCompat {
     }
 
     public static void runAtLocation(Plugin plugin, Location location, Runnable runnable) {
-        if (IS_FOLIA) {
-            try {
-                Object scheduler = getRegionScheduler.invoke(Bukkit.getServer());
-                regionRun.invoke(scheduler, plugin, location, runnable);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        } else {
-            Bukkit.getScheduler().runTask(plugin, runnable);
-        }
+        run(plugin, runnable);
     }
 
     public static void runDelayedAtLocation(Plugin plugin, Location location, Runnable runnable, long delay) {
-        if (IS_FOLIA) {
-            try {
-                Object scheduler = getRegionScheduler.invoke(Bukkit.getServer());
-                regionRunDelayed.invoke(scheduler, plugin, location, runnable, delay);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        } else {
-            Bukkit.getScheduler().runTaskLater(plugin, runnable, delay);
-        }
+        run(plugin, runnable);
     }
 
     public static void runForPlayer(Plugin plugin, Player player, Runnable runnable) {
-        if (IS_FOLIA) {
-            try {
-                Object scheduler = getRegionScheduler.invoke(Bukkit.getServer());
-                regionRun.invoke(scheduler, plugin, player.getLocation(), runnable);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        } else {
-            Bukkit.getScheduler().runTask(plugin, runnable);
-        }
+        run(plugin, runnable);
     }
 
     public static void cancelTask(Plugin plugin, int taskId) {
@@ -138,29 +85,19 @@ public final class FoliaCompat {
     }
 
     public static void scheduleAtFixedRateAsync(Plugin plugin, Runnable runnable, long initialDelay, long period, TimeUnit unit) {
-        if (IS_FOLIA) {
-            try {
-                Object scheduler = getAsyncScheduler.invoke(Bukkit.getServer());
-                asyncRunAtFixedRate.invoke(scheduler, plugin, runnable, period, period, unit);
-            } catch (Exception e) {
-                e.printStackTrace();
+        ASYNC_EXECUTOR.scheduleAtFixedRate(() -> {
+            if (plugin.isEnabled()) {
+                try {
+                    runnable.run();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
-        } else {
-            Bukkit.getScheduler().runTaskTimerAsynchronously(plugin, runnable, initialDelay, period);
-        }
+        }, unit.toMillis(initialDelay), unit.toMillis(period), TimeUnit.MILLISECONDS);
     }
 
     public static void executeGlobal(Plugin plugin, Runnable runnable) {
-        if (IS_FOLIA) {
-            try {
-                Object scheduler = getGlobalRegionScheduler.invoke(Bukkit.getServer());
-                globalExecute.invoke(scheduler, plugin, runnable);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        } else {
-            Bukkit.getScheduler().runTask(plugin, runnable);
-        }
+        run(plugin, runnable);
     }
 
     private FoliaCompat() {
